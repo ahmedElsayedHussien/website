af2791c008dda5517682f2d8b9db629a58bbf448
ahmedElsayedHussien <ahbezo4@gmail.com>
2026-02-15 23:07:19 +0200

    Install Django and other Python packages into the virtual environment.

---
+++ promasky_platform/core/marketing/urls.py
@@ -0,0 +1,8 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('', views.home, name='home'),
+    path('pricing/', views.pricing, name='pricing'),
+    path('trial/', views.trial, name='trial'),
+]
---
+++ promasky_platform/core/marketing/views.py
@@ -0,0 +1,11 @@
+# marketing/views.py
+from django.shortcuts import render
+
+def home(request):
+    return render(request, 'marketing/index.html')
+
+def pricing(request):
+    return render(request, 'marketing/pricing.html')
+
+def trial(request):
+    return render(request, 'marketing/trial.html')
